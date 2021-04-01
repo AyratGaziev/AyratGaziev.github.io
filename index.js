91b17c1f4d4c94498520151ae1783eb334317368
Ayrat Gazievm <klub724@gmail.com>
2021-04-01 15:42:23 +0500

    animation corrected

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const animElements = document.querySelectorAll(".animte");
+const email = document.querySelector("#email");
+const telegram = document.querySelector("#telegram");
+const github = document.querySelector("#github");
+
 const observer = new IntersectionObserver(
     (entries) => {
         entries.forEach((el) => {
@@ -16,4 +20,12 @@ const observer = new IntersectionObserver(
 );
 animElements.forEach((el) => {
     observer.observe(el);
+});
+
+email.addEventListener("transitionend", () => {
+    console.log("work");
+    telegram.classList.add("animated");
+});
+telegram.addEventListener("transitionend", () => {
+    github.classList.add("animated");
 });
